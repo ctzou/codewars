23f4fea1a10f51cd6071994dbfca1628dec9471f
ctzou <6112684+ctzou@users.noreply.github.com>
2022-04-16 06:35:02 -0700

    more codewars

---
+++ Strings/Are You Playing Banjo.js
@@ -0,0 +1,5 @@
+//https://www.codewars.com/kata/53af2b8861023f1d88000832/javascript
+
+function areYouPlayingBanjo(name) {
+   return (name.charAt(0).toLowerCase()==='r') ? `${name} plays banjo` : `${name} does not play banjo`
+ }
--- Strings/DNA to RNA Conversion.js
+++ Strings/DNA to RNA Conversion.js
@@ -1,16 +1,39 @@
-//link
+//https://www.codewars.com/kata/5556282156230d0e5e000089/javascript
 
 function DNAtoRNA(dna) {
+   if (dna.length > 0){
+      return dna.replaceAll("T","U")
+    } else {
+      return dna
+    }
+  }
+
+   //if not null, split, forEach if arrary[i]===T, assign U
+   /*let dnArray=[]
+   if (dna.length >0){
+      //console.log(dna.length)
+      dnArray = dna.split('')
+      //console.log(dnArray)
+      return dnArray.map((letter,idx)=>{
+         if(letter==='T'){ 
+            dnArray[idx]='U';
+         }}).join('')
+      }
+   }
+   console.log(DNAtoRNA("TTTT"))*/
+
+/*function DNAtoRNA(dna) {
    //if not null, split, forEach if arrary[i]===T, assign U
    if (dna.length >0){
       console.log(dna.length)
-     return dna.split('').forEach((letter,idx)=>{
-       if(letter==='T') {
-         dna[idx]==='U'
-       }})
-   } else{
-      return
+      return dna.split('').map((letter,idx)=>{
+         if(letter==='T') {
+            console.log('Tee');
+         dna[idx]='U';
+         console.log(dna[idx])
+         } 
+      })
    }
- }
-
+   }*/
  console.log(DNAtoRNA("TTTT"))
+ // incomplete
