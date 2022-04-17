077ab0d9bd1a7ad5ae29b0e2dc24ee2c7cc9e8a5
ctzou <6112684+ctzou@users.noreply.github.com>
2022-04-17 08:38:16 -0700

    add codewars

---
+++ Strings/Convert a string to an array.js
@@ -0,0 +1,5 @@
+//https://www.codewars.com/kata/57e76bc428d6fbc2d500036d/javascript
+
+function stringToArray(string){
+   string.split(' ')
+ }
---
+++ Strings/Sum Mixed Array.js
@@ -0,0 +1,5 @@
+//https://www.codewars.com/kata/57eaeb9578748ff92a000009
+
+function sumMix(x){
+   return x.map(Number).reduce((a,b)=>a+b,0)
+ }
